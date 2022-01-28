44cc28041c3a473d94b6db32d1c436d0b33fb93c
millieTime <thisismynewemailsoyeah@gmail.com>
2022-01-28 16:48:40 -0700

    added some class skeletons

---
+++ question.py
@@ -0,0 +1,19 @@
+class Question:
+
+    def __init__(self):
+        pass
+
+    def to_string(self):
+        pass
+
+    def check_answer(self, user_answer):
+        pass
+
+    def get_first_num(self):
+        pass
+
+    def get_second_num(self):
+        pass
+
+    def get_operator(self):
+        pass
---
+++ questionGenerator.py
@@ -0,0 +1,48 @@
+# Generates questions for the math game.
+class QuestionGenerator:
+
+    def __init__(self, difficulty = 0):
+        pass
+
+    def generate_sum_question(self):
+        pass
+
+    def generate_difference_question(self):
+        pass
+
+    def generate_product_question(self):
+        pass
+
+    def generate_quotient_question(self):
+        pass
+
+    def get_difficulty(self):
+        pass
+
+    def set_difficulty(self):
+        pass
+
+    def increment_difficulty(self):
+        pass
+
+
+private int num1;
+    private int num2;
+    private int ans;
+    private Timer time;
+    private Timer tStart;
+    private int tCount;
+    private int correct;
+    private int tLimit;
+    public boolean add = true;
+    public boolean minus;
+    public boolean mult;
+    public boolean div;
+
+
+    new_question
+    ans = 0;
+		if (add){
+    	    num1 = (int) (Math.random() * 10);
+    	    num2 = (int) (Math.random() * 10);
+		}
