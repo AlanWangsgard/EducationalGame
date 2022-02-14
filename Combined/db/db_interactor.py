809f0de29fad4ce06a35e1a85d484703c11c7852
millieTime <thisismynewemailsoyeah@gmail.com>
2022-02-14 16:20:29 -0700

    The db_interactor is mostly complete

--- Combined/db/db_interactor.py
+++ Combined/db/db_interactor.py
@@ -8,8 +8,8 @@ DIR = str(Path(__file__).resolve().parent)
 
 class DB_Interactor():
     # These are the names of my top-level collections in Firestore.
-    PANDORA_COLLECTION = u"Pandora Song Info"
-    OWNED_COLLECTION = u"Owned Song Info"
+    COLLECTION = u"high_scores"
+    DOCUMENT_KEY = u"score"
     __db = None
 
     def __init__(self, filepath):
@@ -35,74 +35,36 @@ class DB_Interactor():
             print("No songs found")
         else:
             # Print header
-            keys = ["name", "artist", "album", "duration", "owned_status"]
-            print("{:>50}  {:>36}  {:>40} {:>8} {}".format(*keys))
-            # Print songs. Use .get in case a field is missing.
+            print("Score:")
+            # Print all the scores
             for entry in dict_list:
-                print("{:>50.50}  {:>36.36}  {:>40.40} {:^8} {:>6}".format(*[entry.get(key) or "" for key in keys]))
+                print("\t", entry[self.DOCUMENT_KEY])
 
-    def get_all_songs(self, collection_name, display = False):
+    def get_all_scores(self, display = False):
         # Gets all song documents in the database, in alphabetical order.
-        song_collection = self.__db.collection(collection_name)
-        song_docs = [*song_collection.order_by(u"name").order_by(u"artist").get()]
-        if not song_docs:
+        score_collection = self.__db.collection(self.COLLECTION)
+        score_docs = [*score_collection.order_by(u"score").get()]
+        if not score_docs:
             print("No songs found")
         elif display:
-            self.display_doc_list(song_docs)
-        return song_docs
+            self.display_doc_list(score_docs)
+        return score_docs
 
-    def get_songs_that_match(self, song_info, collection_name, display = False):
-        # Gets song documents that match the given song info from the database.
-        if song_info.is_empty():
-            # Nothing to filter by, just return all of them.
-            return self.get_all_songs(collection_name, display)
-        
-        song_collection = self.__db.collection(collection_name)
-        for key in song_info.get_keys():
-            val = song_info.get(key)
-            if val:
-                song_collection = song_collection.where(key, u"==", val)
-        
-        song_docs = [*song_collection.get()]
-        if not song_docs:
-            print("No matches found")
-        elif display:
-            self.display_doc_list(song_docs)
-        return song_docs
+    def add_score(self, score):
+        # Adds a score to the database.
+        self.__db.collection(self.COLLECTION).add({self.DOCUMENT_KEY: score})
 
-    def add_song(self, song_info, collection_name):
-        # Adds a song to the database.
-        self.__db.collection(collection_name).add(song_info.get_data_dict())
+    def update_scores(self, score):
+        # Determine where the user's score fits in to the cloud database and update the cloud scores.
+        # Process:
+        #   Get all the scores from the database and store them in a list.
+        #   Add the user's score to the list.
+        #   Sort the list highest to lowest.
+        #   FOR THE TOP 5 SCORES ONLY:
+        #       update document 1 with the highest score.
+        #       update document 2 with the second highest score.
+        #       Update doc 3 w 3rd, doc 4 w 4th, and doc 5 w 5th.
+        #   End
 
-    def edit_owned(self, search_info, owned):
-        # Updates whether I own a song. 'owned' must be "T" or "F".
-        assert(owned in ["T", "F"])
-        matches = self.get_songs_that_match(search_info, DB_Interactor.PANDORA_COLLECTION)
-        if len(matches) == 0:
-            print("Couldn't update.")
-        elif len(matches) > 1:
-            print("More than one match exists")
-            self.display_doc_list(matches)
-            if input("Update all (y/n)? ") == "y":
-                for song in matches:
-                    song_id = song.id
-                    self.__db.collection(DB_Interactor.PANDORA_COLLECTION).document(song_id).update({u'owned_status': owned})
-        else:
-            song_id = matches[0].id
-            self.__db.collection(DB_Interactor.PANDORA_COLLECTION).document(song_id).update({u'owned_status': owned})
-
-    def remove_song(self, search_info, collection_name):
-        # Removes a song (or songs) from the database.
-        matches = self.get_songs_that_match(search_info, collection_name)
-        if len(matches) == 0:
-            print("Can't delete.")
-        elif len(matches) > 1:
-            print("More than one match exists")
-            self.display_doc_list(matches)
-            if input("Delete all (y/n)? ") == "y":
-                for song in matches:
-                    song_id = song.id
-                    self.__db.collection(collection_name).document(song_id).delete()
-        else:
-            song_id = matches[0].id
-            self.__db.collection(collection_name).document(song_id).delete()
+        # This line of code gets a document and updates the score. Put it in a loop.
+        self.__db.collection(self.COLLECTION).document(document_id_should_be_1_to_5).update({self.DOCUMENT_KEY: score_it_should_be})
