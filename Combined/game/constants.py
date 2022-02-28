b4d6a45a22cb51b447f05fbcdb5f19918b155c91
millieTime <thisismynewemailsoyeah@gmail.com>
2022-02-28 16:12:35 -0700

    making it more mac compatible

--- Combined/game/constants.py
+++ Combined/game/constants.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 DIR = str(Path(__file__).resolve().parent.parent)
-CERT_FILE_PATH = DIR + "\\db\\" + "cert.json" ##### REPLACE WITH YOUR ADMIN SDK CERT FILENAME.
+CERT_FILE_PATH = DIR + "/db/" + "cert.json" ##### REPLACE WITH YOUR ADMIN SDK CERT FILENAME.
 PICTURES_PATH = DIR + "/pictures/"
 
 # car
